f72962f3127a86f1502249392739f29b09e01b95
Will <willusher.life@gmail.com>
2014-08-26 23:31:30 -0600

    Add render target header

---
+++ prj1/include/render/render_target.h
@@ -0,0 +1,52 @@
+#ifndef RENDER_TARGET_H
+#define RENDER_TARGET_H
+
+#include <vector>
+
+//Since we fwrite this struct directly and PPM only takes RGB (24 bits)
+//we can't allow any padding to be added onto the end
+#pragma pack(1)
+struct Color {
+	uint8_t r, g, b;
+};
+
+/*
+ * The render target where pixel data is stored for the rendered scene
+ * along with for some reason a depth buffer is required for proj1?
+ */
+class RenderTarget {
+	size_t width, height;
+	std::vector<Color> color;
+	std::vector<float> depth;
+
+public:
+	/*
+	 * Create a render target with width * height pixels
+	 */
+	RenderTarget(size_t width, size_t height);
+	/*
+	 * Write a color value to the image at pixel(x, y)
+	 */
+	void write_pixel(size_t x, size_t y, const Color &color);
+	/*
+	 * Write a depth value to the depth buffer at pixel(x, y)
+	 */
+	void write_depth(size_t x, size_t y, float d);
+	//Save the image or depth buffer to the desired file
+	bool save_image(const std::string &file) const;
+	bool save_depth(const std::string &file) const;
+	size_t get_width() const;
+	size_t get_height() const;
+
+private:
+	//Generate a normalized luminosity image of the depth buffer
+	void generate_depth_img();
+	/*
+	 * Save data as a PPM image to the file, the PPM will have n_comp
+	 * components per pixel
+	 */
+	bool save_ppm(const std::string &file, int n_comp, uint8_t *data) const;
+};
+
+#endif
+
