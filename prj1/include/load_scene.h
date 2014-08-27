85a17f3df46065c49919fb8e5668ef9f07013e30
Will <willusher.life@gmail.com>
2014-08-27 17:52:47 -0600

    Debug printing of scene loading

--- prj1/include/load_scene.h
+++ prj1/include/load_scene.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <tinyxml2.h>
 #include "linalg/vector.h"
+#include "geometry/geometry.h"
 
 /*
  * Load a scene as described by the XML document and return it
@@ -11,13 +12,18 @@
  */
 void load_scene(const std::string &file);
 /*
+ * Load object nodes in the XMLElement as children of the
+ * passed node
+ */
+void load_node(tinyxml2::XMLElement *elem);
+/*
  * Read the x,y,z attributes of the XMLElement and return it
  */
-Vector read_vector(tinyxml2::XMLElement *elem);
+void read_vector(tinyxml2::XMLElement *elem, Vector &v);
 /*
  * Read the value float attribute of the XMLElement and return it
  */
-float read_value(tinyxml2::XMLElement *elem);
+void read_float(tinyxml2::XMLElement *elem, float &f);
 
 #endif
 
--- prj1/src/load_scene.cpp
+++ prj1/src/load_scene.cpp
@@ -4,11 +4,6 @@
 #include "linalg/vector.h"
 #include "load_scene.h"
 
-/*
- * Load a scene as described by the XML document and return it
- * TODO: Write scene class, and return it instead of void
- * TODO: throw on these errors?
- */
 void load_scene(const std::string &file){
 	using namespace tinyxml2;
 	XMLDocument doc;
@@ -32,26 +27,46 @@ void load_scene(const std::string &file){
 		std::cerr << "load_scene Error: no camera found\n";
 		return;
 	}
+	load_node(scene);
 }
-/*
- * Read the x,y,z attributes of the XMLElement and return it
- */
-Vector read_vector(tinyxml2::XMLElement *elem){
-	if (elem->Attribute("x") && elem->Attribute("y") && elem->Attribute("z")){
-		return Vector{elem->FloatAttribute("x"), elem->FloatAttribute("y"),
-			elem->FloatAttribute("z")};
+void load_node(tinyxml2::XMLElement *elem){
+	using namespace tinyxml2;
+	//auto &children = node.get_children();
+	for (XMLNode *c = elem->FirstChild(); c; c = c->NextSibling()){
+		if (c->Value() == std::string{"object"}){
+			XMLElement *e = c->ToElement();
+			std::string name = e->Attribute("name");
+			const char *t = e->Attribute("type");
+			std::cout << "Object with name " << name << " found\n";
+			if (t){
+				std::string type = t;
+				std::cout << "type: " << type << std::endl;
+			}
+			//This would really be the child node
+			load_node(e);
+		}
+		else if (c->Value() == std::string{"scale"}){
+			//Query both uniform and non-uniform scaling possibilities
+			float f = 1;
+			Vector v{1, 1, 1};
+			read_float(c->ToElement(), f);
+			read_vector(c->ToElement(), v);
+			v *= f;
+			std::cout << "Scaling of " << v << " applied\n";
+		}
+		else if (c->Value() == std::string{"translate"}){
+			Vector v;
+			read_vector(c->ToElement(), v);
+			std::cout << "Translation of " << v << " applied\n";
+		}
 	}
-	std::cerr << "load_scene Error: malformed vector attribute in scene\n";
-	return Vector{0, 0, 0};
 }
-/*
- * Read the value float attribute of the XMLElement and return it
- */
-float read_value(tinyxml2::XMLElement *elem){
-	if (elem->Attribute("value")){
-		return elem->FloatAttribute("value");
-	}
-	std::cerr << "load_scene Error: malformed value attribute in scene\n";
-	return 0;
+void read_vector(tinyxml2::XMLElement *elem, Vector &v){
+	elem->QueryFloatAttribute("x", &v.x);
+	elem->QueryFloatAttribute("y", &v.y);
+	elem->QueryFloatAttribute("z", &v.z);
+}
+void read_float(tinyxml2::XMLElement *elem, float &f){
+	elem->QueryFloatAttribute("value", &f);
 }
 
