6ef41556acb5760a2f6b9f0bde0435433f62f999
Will <willusher.life@gmail.com>
2014-08-29 23:39:44 -0600

    Working on computing dimensions needed for subrects
    Each subrect will be handed off to an independent thread to be rendered

--- prj1/src/main.cpp
+++ prj1/src/main.cpp
@@ -23,6 +23,14 @@ int main(int argc, char **argv){
 		std::cerr << "Error: No output filename passed\n";
 		return 1;
 	}
+	int n_threads = 1;
+	if (flag(argv, argv + argc, "-n")){
+		n_threads = get_param<int>(argv, argv + argc, "-n");
+		if (n_threads > 1 && n_threads % 2 != 0){
+			std::cerr << "Warning: num threads not even, increasing thread count by 1\n";
+			++n_threads;
+		}
+	}
 	std::string scene_file = get_param<std::string>(argv, argv + argc, "-f");
 	std::string out_file = get_param<std::string>(argv, argv + argc, "-o");
 	std::cout << "scene file: " << scene_file << ", output: " << out_file << std::endl;
@@ -31,6 +39,17 @@ int main(int argc, char **argv){
 	Node &root = scene.get_root();
 	RenderTarget &target = scene.get_render_target();
 	Camera &camera = scene.get_camera();
+
+	//Compute the number of columns and rows to partition the image into so
+	//we can assign disjoint portions of it to each thread
+	//Should warn if the cols & rows don't evenly divide the space,
+	//eg if rect dimensions aren't integers
+	int n_cols = std::ceil(std::sqrt(n_threads));
+	int n_rows = std::ceil(n_threads / static_cast<float>(n_cols));
+	std::cout << "cols: " << n_cols << " rows: " << n_rows
+		<< "\nEach rect is " << target.get_width() / static_cast<float>(n_cols)
+		<< " x " << target.get_height() / static_cast<float>(n_rows)
+		<< std::endl;
 
 	for (size_t y = 0; y < target.get_height(); ++y){
 		for (size_t x = 0; x < target.get_width(); ++x){
