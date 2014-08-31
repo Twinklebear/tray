8f58bf1607193c886b3f3a7ca3d086ab95a3aa6a
Will <willusher.life@gmail.com>
2014-08-30 23:47:01 -0600

    Remove load_texture declaration

--- prj1/previewer/util.h
+++ prj1/previewer/util.h
@@ -17,14 +17,6 @@ namespace util {
 	 */
 	GLint load_program(const std::vector<std::tuple<GLenum, std::string>> &shaders);
 	/*
-	 * Load an image into a 2D texture, creating a new texture id
-	 * The texture unit desired for this texture should be set active
-	 * before loading the texture as it will be bound during the loading process
-	 * Can also optionally pass width & height variables to return the width
-	 * and height of the loaded image
-	 */
-	GLuint load_texture(const std::string &file, size_t *width = nullptr, size_t *height = nullptr);
-	/*
 	 * Check for an OpenGL error and log it along with the message passed
 	 * if an error occured. Will return true if an error occured & was logged
 	 */
@@ -42,3 +34,4 @@ namespace util {
 }
 
 #endif
+
