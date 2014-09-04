57ba141201900faca6a9100f74ce3ae9d042c7d0
Will Usher <willusher.life@gmail.com>
2014-09-04 10:33:18 -0600

    Comment fixing

--- include/cache.h
+++ include/cache.h
@@ -15,13 +15,13 @@ public:
 	using iter = typename std::unordered_map<std::string, std::unique_ptr<T>>::iterator;
 	using citer = typename std::unordered_map<std::string, std::unique_ptr<T>>::const_iterator;
 	/*
-	 * Add some geometry to the cache
+	 * Add some T to the cache
 	 */
 	void add(const std::string &name, std::unique_ptr<T> t){
 		cache[name] = std::move(t);
 	}
 	/*
-	 * Get a non-owning pointer to some geometry in the cache
+	 * Get a non-owning pointer to some T in the cache
 	 * nullptr is returned if the name isn't found
 	 */
 	const T* get(const std::string &name) const {
