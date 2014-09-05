fa022fb420989c05c1b9d37576626ad21a128a7a
Will Usher <willusher.life@gmail.com>
2014-09-05 16:24:35 -0600

    Lights now used in blinn phong, but broken

--- src/material/blinn_phong.cpp
+++ src/material/blinn_phong.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "lights/light.h"
 #include "material/blinn_phong.h"
 
@@ -5,6 +6,24 @@ BlinnPhong::BlinnPhong(const Colorf &diffuse, const Colorf &specular, float glos
 	: diffuse(diffuse), specular(specular), gloss(gloss)
 {}
 Colorf BlinnPhong::shade(const Ray &r, const HitInfo &hitinfo, const LightCache &lights) const {
-	return diffuse;
+	Colorf illum;
+	for (const auto &lit : lights){
+		const auto &light = lit.second;
+		if (light->is_ambient()){
+			illum += diffuse * light->illuminate(hitinfo.point);
+		}
+		else {
+			Vector l = -(light->direction(hitinfo.point)).normalized();
+			Vector v = -r.d.normalized();
+			Vector h = (l + v).normalized();
+			//Normal may not be normalized due to translation into world space
+			Normal n = hitinfo.normal.normalized();
+			float dif = std::max(l.dot(n), 0.f);
+			float spec = std::pow(std::max(n.dot(h), 0.f), gloss);
+			illum += (diffuse * dif + specular * spec) * light->illuminate(hitinfo.point);
+		}
+	}
+	illum.normalize();
+	return illum;
 }
 
