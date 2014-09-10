74667804993d9a2bd26539c62cd91f5b98da0bb6
Will <willusher.life@gmail.com>
2014-09-09 22:29:45 -0600

    Toss light if diff component is 0 (eg l dot n <= 0 -> no light)

--- src/material/blinn_phong.cpp
+++ src/material/blinn_phong.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cmath>
 #include "lights/light.h"
 #include "material/blinn_phong.h"
@@ -18,6 +19,9 @@ Colorf BlinnPhong::shade(const Ray &r, const HitInfo &hitinfo, const std::vector
 			//Normal may not be normalized due to translation into world space
 			Normal n = hitinfo.normal.normalized();
 			float dif = std::max(l.dot(n), 0.f);
+			if (dif == 0.f){
+				continue;
+			}
 			float spec = std::pow(std::max(n.dot(h), 0.f), gloss);
 			illum += diffuse * dif * light->illuminate(hitinfo.point)
 				+ specular * spec * light->illuminate(hitinfo.point);
