d714bc7d9cc21c157ccbcae5733a48d8714cdda0
Will Usher <willusher.life@gmail.com>
2014-09-26 14:01:29 -0600

    Just starting to work on texture mapping generators

---
+++ include/textures/texture_mapping.h
@@ -0,0 +1,34 @@
+#ifndef TEXTURE_MAPPING_H
+#define TEXTURE_MAPPING_H
+
+#include "geometry/differential_geometry.h"
+#include "linalg/vector.h"
+
+/*
+ * Stores information about the s,t coords and derivatives
+ * to evaluat a texture's value
+ */
+struct TextureSample {
+	float s, t, ds_dx, ds_dy, dt_dx, dt_dy;
+
+	TextureSample(float s = 0, float t = 0, float ds_dx = 0,
+		float ds_dy = 0, float dt_dx = 0, float dt_dy = 0);
+};
+
+/*
+ * Interface for mapping u,v coords to s,t texture coords
+ */
+class TextureMapping {
+	Vector scale, translate;
+
+public:
+	TextureMapping(const Vector &scale, const Vector &translate);
+	/*
+	 * Compute the texture sample position and derivatives for the
+	 * differential geometry being rendered
+	 */
+	virtual TextureSample map(const DifferentialGeometry &dg) const = 0;
+};
+
+#endif
+
---
+++ src/textures/texture_mapping.cpp
@@ -0,0 +1,12 @@
+#include "geometry/differential_geometry.h"
+#include "linalg/vector.h"
+#include "textures/texture_mapping.h"
+
+TextureSample::TextureSample(float s, float t, float ds_dx, float ds_dy, float dt_dx, float dt_dy)
+	: s(s), t(t), ds_dx(ds_dx), ds_dy(ds_dy), dt_dx(dt_dx), dt_dy(dt_dy)
+{}
+
+TextureMapping::TextureMapping(const Vector &scale, const Vector &translate)
+	: scale(scale), translate(translate)
+{}
+
