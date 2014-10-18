eb74613cd6e99dc3f30637d062510bac32ca847a
Will <willusher.life@gmail.com>
2014-10-18 16:31:06 -0600

    Cleaning up some comments

--- src/linalg/ray.cpp
+++ src/linalg/ray.cpp
@@ -4,14 +4,14 @@
 #include "linalg/ray.h"
 
 RayDifferential RayDifferential::reflect(const DifferentialGeometry &dg) const {
-	//wo = -ray.d
-	//wi is the reflected direction of the main ray, ie. refl.d
 	Vector n{dg.normal.normalized()};
 	Vector dir = d - 2 * n.dot(d) * n;
 	RayDifferential refl{dg.point, dir.normalized(), *this, 0.001};
 	if (has_differentials()){
 		refl.rx = Ray{refl.o + dg.dp_dx, refl.d, *this, 0.001};
 		refl.ry = Ray{refl.o + dg.dp_dy, refl.d, *this, 0.001};
+		//We compute dn_dx and dn_dy as described in PBR since we're following their differential
+		//geometry method, the rest of the computation is directly from Igehy's paper
 		auto dn_dx = Vector{dg.dn_du * dg.du_dx + dg.dn_dv * dg.dv_dx};
 		auto dn_dy = Vector{dg.dn_du * dg.du_dy + dg.dn_dv * dg.dv_dy};
 		auto dd_dx = -rx.d + d;
@@ -33,13 +33,14 @@ RayDifferential RayDifferential::refract(const DifferentialGeometry &dg, const V
 	if (has_differentials()){
 		refr.rx = Ray{refr.o + dg.dp_dx, refr.d, *this, 0.001};
 		refr.ry = Ray{refr.o + dg.dp_dy, refr.d, *this, 0.001};
+		//We compute dn_dx and dn_dy as described in PBR since we're following their differential
+		//geometry method, the rest of the computation is directly from Igehy's paper
 		auto dn_dx = Vector{dg.dn_du * dg.du_dx + dg.dn_dv * dg.dv_dx};
 		auto dn_dy = Vector{dg.dn_du * dg.du_dy + dg.dn_dv * dg.dv_dy};
 		auto dd_dx = -rx.d + d;
 		auto dd_dy = -ry.d + d;
 		float ddn_dx = dd_dx.dot(n) - d.dot(dn_dx);
 		float ddn_dy = dd_dy.dot(n) - d.dot(dn_dy);
-		//In the paper D = incident ray dir, D' = refracted dir
 		float mu = eta * d.dot(n) - refr.d.dot(n);
 		float dmu_dx = (eta - eta * eta * d.dot(n) / refr.d.dot(n)) * ddn_dx;
 		float dmu_dy = (eta - eta * eta * d.dot(n) / refr.d.dot(n)) * ddn_dy;
