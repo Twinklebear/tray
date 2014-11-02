a40857f7e6a7b0a425b0d8051c3a0f69cce7c510
Will <willusher.life@gmail.com>
2014-11-02 16:23:47 -0700

    Add BSDF type described by PBR

---
+++ include/material/bsdf.h
@@ -0,0 +1,67 @@
+#ifndef BSDF_H
+#define BSDF_H
+
+#include <random>
+#include <memory>
+#include <vector>
+#include "linalg/vector.h"
+#include "geometry/differential_geometry.h"
+#include "film/color.h"
+#include "bxdf.h"
+
+/*
+ * Implementation of BSDF to manage surface's BRDT and BTDFs motivated by PBR
+ */
+class BSDF {
+	//Vectors forming the shading coordinate system:
+	//shading normal, secondary and primary tangents along with the geometry normal
+	//the coordinate system has the shading normal as z, primary tangent as x and secondary as y
+	Normal ns, ng;
+	Vector s, t;
+	std::vector<std::unique_ptr<BxDF>> bxdfs;
+
+public:
+	const DifferentialGeometry dg;
+	const float eta;
+
+	/*
+	 * Construct the BSDF for the differential geometry being shaded
+	 */
+	BSDF(const DifferentialGeometry &dg, const Normal &ng, float eta);
+	/*
+	 * Add a BxDF to the set of BxDFs used by this BSDF
+	 */
+	void add(std::unique_ptr<BxDF> b);
+	/*
+	 * Return the number of BxDFs in this BSDF or those specifically matching a flag
+	 */
+	int num_bxdfs() const;
+	int num_bxdfs(BxDFTYPE flags) const;
+	/*
+	 * Transform to and from the shading space used by the BxDFs
+	 */
+	Vector to_shading(const Vector &v) const;
+	Vector from_shading(const Vector &v) const;
+	/*
+	 * Evaluate the BxDFs at the surface for the incoming and outgoing light directions
+	 * light directions should be in world space
+	 * Can optionally specify BxDFTYPE flag filters, default is all
+	 */
+	Colorf operator()(const Vector &wo_world, const Vector &wi_world, BxDFTYPE flags = BxDFTYPE::ALL) const;
+	/*
+	 * Sample the hemispherical-directional reflectance function of the BxDFs
+	 * sqrt_samples is the square root of the number of samples to take
+	 */
+	Colorf rho_hd(const Vector &wo, std::minstd_rand &rng, BxDFTYPE flags = BxDFTYPE::ALL, int sqrt_samples = 6) const;
+	/*
+	 * Sample the hemispherical-hemispherical reflectance function of the BxDFs
+	 */
+	Colorf rho_hh(std::minstd_rand &rng, BxDFTYPE flags = BxDFTYPE::ALL, int sqrt_samples = 6) const;
+	/*
+	 * Compute the probability density function for BxDFs for the directions passed
+	 */
+	float pdf(const Vector &wo_world, const Vector &wi_world, BxDFTYPE flags = BxDFTYPE::ALL) const;
+};
+
+#endif
+
---
+++ src/material/bsdf.cpp
@@ -0,0 +1,86 @@
+#include "samplers/stratified_sampler.h"
+#include "material/bsdf.h"
+
+BSDF::BSDF(const DifferentialGeometry &dg, const Normal &ng, float eta)
+	: ns(dg.normal), ng(ng), s(dg.dp_du.normalized()), t(ns.cross(s)), dg(dg), eta(eta)
+{}
+void BSDF::add(std::unique_ptr<BxDF> b){
+	bxdfs.push_back(std::move(b));
+}
+int BSDF::num_bxdfs() const {
+	return bxdfs.size();
+}
+int BSDF::num_bxdfs(BxDFTYPE flags) const {
+	int n = 0;
+	for (const auto &b : bxdfs){
+		if (b->matches(flags)){
+			++n;
+		}
+	}
+	return n;
+}
+Vector BSDF::to_shading(const Vector &v) const {
+	return Vector{v.dot(s), v.dot(t), v.dot(ns)};
+}
+Vector BSDF::from_shading(const Vector &v) const {
+	return Vector{s.x * v.x + t.x * v.y + ns.x * v.z,
+		s.y * v.x + t.y * v.y + ns.y * v.z,
+		s.z * v.x + t.z * v.y + ns.z * v.z};
+}
+Colorf BSDF::operator()(const Vector &wo_world, const Vector &wi_world, BxDFTYPE flags) const {
+	Vector wo = to_shading(wo_world);
+	Vector wi = to_shading(wi_world);
+	//Determine if we should be evaluating reflection or transmission based on the geometry normal
+	if (wo_world.dot(ng) * wi_world.dot(ng) > 0){
+		flags = BxDFTYPE(flags & ~BxDFTYPE::TRANSMISSION);
+	}
+	else {
+		flags = BxDFTYPE(flags & ~BxDFTYPE::REFLECTION);
+	}
+	Colorf color;
+	for (const auto &b : bxdfs){
+		if (b->matches(flags)){
+			color += (*b)(wo, wi);
+		}
+	}
+	return color;
+}
+Colorf BSDF::rho_hd(const Vector &wo, std::minstd_rand &rng, BxDFTYPE flags, int sqrt_samples) const {
+	std::vector<std::array<float, 2>> samples(sqrt_samples * sqrt_samples);
+	StratifiedSampler::sample2d(samples, rng);
+	Colorf color;
+	for (const auto &b : bxdfs){
+		if (b->matches(flags)){
+			color += b->rho_hd(wo, samples);
+		}
+	}
+	return color;
+}
+Colorf BSDF::rho_hh(std::minstd_rand &rng, BxDFTYPE flags, int sqrt_samples) const {
+	std::vector<std::array<float, 2>> samples_a(sqrt_samples * sqrt_samples);
+	std::vector<std::array<float, 2>> samples_b(sqrt_samples * sqrt_samples);
+	StratifiedSampler::sample2d(samples_a, rng);
+	StratifiedSampler::sample2d(samples_b, rng);
+	Colorf color;
+	for (const auto &b : bxdfs){
+		if (b->matches(flags)){
+			color += b->rho_hh(samples_a, samples_b);
+		}
+	}
+	return color;
+
+}
+float BSDF::pdf(const Vector &wo_world, const Vector &wi_world, BxDFTYPE flags) const {
+	Vector wo = to_shading(wo_world);
+	Vector wi = to_shading(wi_world);
+	float pdf_val = 0;
+	int n_comps = 0;
+	for (const auto &b : bxdfs){
+		if (b->matches(flags)){
+			++n_comps;
+			pdf_val += b->pdf(wo, wi);
+		}
+	}
+	return n_comps > 0 ? pdf_val / n_comps : 0;
+}
+
