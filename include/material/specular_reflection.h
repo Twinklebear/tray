60e5cc546d7eedccbf3243656616ceb41dad72bd
Will <willusher.life@gmail.com>
2014-11-02 10:48:56 -0700

    Add BRDF for perfect specular reflection

---
+++ include/material/specular_reflection.h
@@ -0,0 +1,40 @@
+#ifndef SPECULAR_REFLECTION_H
+#define SPECULAR_REFLECTION_H
+
+#include <memory>
+#include "fresnel.h"
+#include "bxdf.h"
+
+/*
+ * BRDF describing perfectly specular reflection, eg. a delta distribution
+ */
+class SpecularReflection : public BxDF {
+	Colorf reflection;
+	std::unique_ptr<Fresnel> fresnel;
+
+public:
+	/*
+	 * Create a specularly reflective BRDF with some reflective color
+	 * and desired Fresnel component
+	 */
+	SpecularReflection(const Colorf &reflection, std::unique_ptr<Fresnel> fresnel);
+	/*
+	 * Compute the value of the BxDF for some incident and outgoing directions
+	 * Note that because this is a delta distribution this will always return 0,
+	 * sample must be used for delta distributions instead
+	 */
+	Colorf operator()(const Vector &wo, const Vector &wi) const override;
+	/*
+	 * Sample the BRDFs value for some outgoing direction using the random values
+	 * passed and returning the incident light direction
+	 */
+	Colorf sample(const Vector &wo, Vector &wi, const std::array<float, 2> &samples, float &pdf_val) const override;
+	/*
+	 * Compute the probability density function for sampling the directions passed
+	 * Note again that this returns 0 because this is a delta distribution
+	 */
+	float pdf(const Vector &wo, const Vector &wi) const override;
+};
+
+#endif
+
---
+++ src/material/specular_reflection.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include "material/specular_reflection.h"
+
+SpecularReflection::SpecularReflection(const Colorf &reflection, std::unique_ptr<Fresnel> fresnel)
+	: BxDF(BxDFTYPE(BxDFTYPE::REFLECTION | BxDFTYPE::SPECULAR)), reflection(reflection), fresnel(std::move(fresnel))
+{}
+Colorf SpecularReflection::operator()(const Vector&, const Vector&) const {
+	return Colorf{0};
+}
+Colorf SpecularReflection::sample(const Vector &wo, Vector &wi, const std::array<float, 2>&, float &pdf_val) const {
+	//Compute the perfect specular reflection direction since we're explicitly sampling the delta distribution
+	wi = Vector{-wo.x, -wo.y, wo.z};
+	pdf_val = 1;
+	return (*fresnel)(cos_theta(wo)) * reflection / std::abs(cos_theta(wi));
+}
+float SpecularReflection::pdf(const Vector&, const Vector&) const {
+	return 0;
+}
+
