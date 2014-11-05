76ffc045ad959998c3cf85230da06def6279cd26
Will <willusher.life@gmail.com>
2014-11-04 22:49:09 -0700

    Fix occlusion tester segment ray creation to behave like before

--- src/lights/occlusion_tester.cpp
+++ src/lights/occlusion_tester.cpp
@@ -3,11 +3,10 @@
 #include "lights/occlusion_tester.h"
 
 void OcclusionTester::set_points(const Point &a, const Point &b){
-	float dist = a.distance(b);
-	ray = Ray{a, (b - a) / dist, 1e-6f, dist * (1.f - 1e-6f)};
+	ray = Ray{a, b - a, 1e-4f, 1};
 }
 void OcclusionTester::set_ray(const Point &p, const Vector &d){
-	ray = Ray{p, d.normalized(), 1e-6f};
+	ray = Ray{p, d.normalized(), 1e-4f};
 }
 bool OcclusionTester::occluded(const Scene &scene){
 	DifferentialGeometry dg;
