e77bf8392db92deb582ff438a7145b3d3b06428c
Will Usher <willusher.life@gmail.com>
2014-11-06 15:12:20 -0700

    Add PlasticMaterial to use TorranceSparrow model

--- src/material/plastic_material.cpp
+++ src/material/plastic_material.cpp
@@ -1,11 +1,23 @@
 #include <memory>
 #include "film/color.h"
+#include "material/bsdf.h"
+#include "material/blinn_distribution.h"
+#include "material/torrance_sparrow.h"
+#include "material/lambertian.h"
+#include "material/fresnel.h"
 #include "material/plastic_material.h"
 
 PlasticMaterial::PlasticMaterial(const Texture *diffuse, const Texture *specular, float roughness)
 	: diffuse(diffuse), specular(specular), roughness(roughness)
 {}
 BSDF PlasticMaterial::get_bsdf(const DifferentialGeometry &dg) const {
-	//TODO: Need Blinn Microfacet BRDF for this material
+	BSDF bsdf{dg};
+	Colorf kd = diffuse->sample(dg).normalized();	
+	Colorf ks = specular->sample(dg).normalized();
+	bsdf.add(std::make_unique<Lambertian>(kd));
+	bsdf.add(std::make_unique<TorranceSparrow>(ks,
+		std::make_unique<FresnelDielectric>(1.5, 1),
+		std::make_unique<BlinnDistribution>(1.f / roughness)));
+	return bsdf;
 }
 
