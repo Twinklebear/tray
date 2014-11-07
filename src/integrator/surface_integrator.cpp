1521a1ff186fc594b216287e6ce5a50d0af2755f
Will <willusher.life@gmail.com>
2014-11-06 21:12:17 -0700

    Clean up spec_reflect and spec_transmit

--- src/integrator/surface_integrator.cpp
+++ src/integrator/surface_integrator.cpp
@@ -5,60 +5,79 @@ Colorf SurfaceIntegrator::spec_reflect(const RayDifferential &ray, const BSDF &b
 {
 	const Normal &n = bsdf.dg.normal;
 	const Point &p = bsdf.dg.point;
-	Vector wo = -ray.d;
-	Vector wi;
+	Vector w_o = -ray.d;
+	Vector w_i;
 	float pdf_val = 0;
 	std::vector<std::array<float, 2>> u_sample(1);
 	std::vector<float> c_sample(1);
 	sampler.get_samples(u_sample);
 	sampler.get_samples(c_sample);
-	Colorf f = bsdf.sample(wo, wi, u_sample[0], c_sample[0], pdf_val,
+	Colorf f = bsdf.sample(w_o, w_i, u_sample[0], c_sample[0], pdf_val,
 		BxDFTYPE(BxDFTYPE::REFLECTION | BxDFTYPE::SPECULAR));	
 	//Compute the color reflected off the BSDF
 	Colorf reflected{0};
-	if (pdf_val > 0 && !f.luminance() == 0 && std::abs(wi.dot(n)) != 0){
-		RayDifferential refl{p, wi, ray, 0.001};
+	if (pdf_val > 0 && !f.luminance() == 0 && std::abs(w_i.dot(n)) != 0){
+		RayDifferential refl{p, w_i, ray, 0.001};
 		if (ray.has_differentials()){
-			refl.rx = Ray{p + bsdf.dg.dp_dx, wi, ray, 0.001};
-			refl.ry = Ray{p + bsdf.dg.dp_dy, wi, ray, 0.001};
+			refl.rx = Ray{p + bsdf.dg.dp_dx, w_i, ray, 0.001};
+			refl.ry = Ray{p + bsdf.dg.dp_dy, w_i, ray, 0.001};
 			//We compute dn_dx and dn_dy as described in PBR since we're following their differential
 			//geometry method, the rest of the computation is directly from Igehy's paper
 			auto dn_dx = Vector{bsdf.dg.dn_du * bsdf.dg.du_dx + bsdf.dg.dn_dv * bsdf.dg.dv_dx};
 			auto dn_dy = Vector{bsdf.dg.dn_du * bsdf.dg.du_dy + bsdf.dg.dn_dv * bsdf.dg.dv_dy};
-			auto dd_dx = -ray.rx.d - wo;
-			auto dd_dy = -ray.ry.d - wo;
-			float ddn_dx = dd_dx.dot(n) + wo.dot(dn_dx);
-			float ddn_dy = dd_dy.dot(n) + wo.dot(dn_dy);
-			refl.rx.d = wi - dd_dx + 2 * Vector{wo.dot(n) * dn_dx + Vector{ddn_dx * n}};
-			refl.ry.d = wi - dd_dy + 2 * Vector{wo.dot(n) * dn_dy + Vector{ddn_dy * n}};
+			auto dd_dx = -ray.rx.d - w_o;
+			auto dd_dy = -ray.ry.d - w_o;
+			float ddn_dx = dd_dx.dot(n) + w_o.dot(dn_dx);
+			float ddn_dy = dd_dy.dot(n) + w_o.dot(dn_dy);
+			refl.rx.d = w_i - dd_dx + 2 * Vector{w_o.dot(n) * dn_dx + Vector{ddn_dx * n}};
+			refl.ry.d = w_i - dd_dy + 2 * Vector{w_o.dot(n) * dn_dy + Vector{ddn_dy * n}};
 		}
 		Colorf li = renderer.illumination(refl, scene, sampler);
-		reflected = f * li * std::abs(wi.dot(n)) / pdf_val;
+		reflected = f * li * std::abs(w_i.dot(n)) / pdf_val;
 	}
 	return reflected;
 }
 Colorf SurfaceIntegrator::spec_transmit(const RayDifferential &ray, const BSDF &bsdf,
 	const Renderer &renderer, const Scene &scene, Sampler &sampler)
 {
-	/*
-	Vector wo = -ray.d;
-	Vector wi;
+	const Normal &n = bsdf.dg.normal;
+	const Point &p = bsdf.dg.point;
+	Vector w_o = -ray.d;
+	Vector w_i;
 	float pdf_val = 0;
 	std::vector<std::array<float, 2>> u_sample(1);
 	std::vector<float> c_sample(1);
 	sampler.get_samples(u_sample);
 	sampler.get_samples(c_sample);
-	Colorf f = bsdf.sample(wo, wi, u_sample[0], c_sample[0], pdf_val,
-		BxDFTYPE(BxDFTYPE::REFLECTION | BxDFTYPE::TRANSMISSION));	
+	Colorf f = bsdf.sample(w_o, w_i, u_sample[0], c_sample[0], pdf_val,
+		BxDFTYPE(BxDFTYPE::TRANSMISSION | BxDFTYPE::SPECULAR));	
 	//Compute the color transmitted through the BSDF
 	Colorf transmitted{0};
-	if (pdf_val > 0 && !f.luminance() == 0 && std::abs(wi.dot(dg.normal)) != 0){
-		RayDifferential refl_ray = ray.refract(dg, bsdf.eta);
-		Colorf li = renderer.illumination(refl_ray, scene, sampler);
-		transmitted = f * li * std::abs(wi.dot(dg.normal)) / pdf_val;
+	if (pdf_val > 0 && !f.luminance() == 0 && std::abs(w_i.dot(n)) != 0){
+		RayDifferential refr_ray{p, w_i, ray, 0.001};
+		if (ray.has_differentials()){
+			refr_ray.rx = Ray{p + bsdf.dg.dp_dx, w_i, ray, 0.001};
+			refr_ray.ry = Ray{p + bsdf.dg.dp_dy, w_i, ray, 0.001};
+
+			float eta = w_o.dot(n) < 0 ? 1 / bsdf.eta : bsdf.eta;
+			//We compute dn_dx and dn_dy as described in PBR since we're following their differential
+			//geometry method, the rest of the computation is directly from Igehy's paper
+			auto dn_dx = Vector{bsdf.dg.dn_du * bsdf.dg.du_dx + bsdf.dg.dn_dv * bsdf.dg.dv_dx};
+			auto dn_dy = Vector{bsdf.dg.dn_du * bsdf.dg.du_dy + bsdf.dg.dn_dv * bsdf.dg.dv_dy};
+			auto dd_dx = -ray.rx.d - w_o;
+			auto dd_dy = -ray.ry.d - w_o;
+			float ddn_dx = dd_dx.dot(n) + w_o.dot(dn_dx);
+			float ddn_dy = dd_dy.dot(n) + w_o.dot(dn_dy);
+
+			float mu = eta * ray.d.dot(n) - w_i.dot(n);
+			float dmu_dx = (eta - eta * eta * ray.d.dot(n) / w_i.dot(n)) * ddn_dx;
+			float dmu_dy = (eta - eta * eta * ray.d.dot(n) / w_i.dot(n)) * ddn_dy;
+			refr_ray.rx.d = w_i + eta * dd_dx - Vector{mu * dn_dx + Vector{dmu_dx * n}};
+			refr_ray.ry.d = w_i + eta * dd_dy - Vector{mu * dn_dy + Vector{dmu_dy * n}};
+		}
+		Colorf li = renderer.illumination(refr_ray, scene, sampler);
+		transmitted = f * li * std::abs(w_i.dot(n)) / pdf_val;
 	}
 	return transmitted;
-	*/
-	return Colorf{0};
 }
 
