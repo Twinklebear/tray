15342e3b7b5a20061f79f87775c7d11dc4493802
Will <willusher.life@gmail.com>
2014-11-06 20:52:13 -0700

    Revert plastic back to be plastic

--- src/material/plastic_material.cpp
+++ src/material/plastic_material.cpp
@@ -2,7 +2,6 @@
 #include "film/color.h"
 #include "material/bsdf.h"
 #include "material/blinn_distribution.h"
-#include "material/anisotropic_distribution.h"
 #include "material/torrance_sparrow.h"
 #include "material/lambertian.h"
 #include "material/fresnel.h"
@@ -18,7 +17,7 @@ BSDF PlasticMaterial::get_bsdf(const DifferentialGeometry &dg) const {
 	bsdf.add(std::make_unique<Lambertian>(kd));
 	bsdf.add(std::make_unique<TorranceSparrow>(ks,
 		std::make_unique<FresnelDielectric>(1.5, 1),
-		std::make_unique<AnisotropicDistribution>(1.f / roughness, 10 / roughness)));
+		std::make_unique<BlinnDistribution>(1.f / roughness)));
 	return bsdf;
 }
 
