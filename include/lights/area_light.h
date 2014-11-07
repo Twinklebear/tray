cecf16a247e669570b2d8bd589241136f0071169
Will Usher <willusher.life@gmail.com>
2014-11-07 16:13:45 -0700

    Add sampling and pdf methods to area light

--- include/lights/area_light.h
+++ include/lights/area_light.h
@@ -22,13 +22,18 @@ public:
 	 */
 	AreaLight(const Transform &to_world, const Colorf &emit, int n_samples, float radius);
 	/*
+	 * Compute the light's emitted radiance in the outgoing direction, w
+	 * from the point on the surface with normal n
+	 */
+	Colorf radiance(const Point &p, const Normal &n, const Vector &w) const;
+	/*
 	 * Sample the illumination from the light arriving at the point
 	 * returns the color along with the incident light direction for the point,
 	 * the PDF of the point that was sampled and fills out the occlusion tester
 	 * for shadow testing if the color returned wasn't black
 	 */
-	Colorf sample(const Point &p, const std::array<float, 2> &lsample,
-		Vector &wi, float &pdf_val, OcclusionTester &occlusion) const override;
+	Colorf sample(const Point &p, const std::array<float, 2> &lsample, Vector &w_i,
+		float &pdf_val, OcclusionTester &occlusion) const override;
 	/*
 	 * Sample a light ray from the distribution of rays leaving the light
 	 * returning the ray, normal on the light surface where the ray was emitted
@@ -47,12 +52,7 @@ public:
 	/*
 	 * Compute the PDF for sampling the point with the incident direction wi
 	 */
-	float pdf(const Point &p, const Vector &wi) const override;
-	/*
-	 * Compute the light's emitted radiance in the outgoing direction, w
-	 * from the point on the surface with normal n
-	 */
-	Colorf radiance(const Point &p, const Normal &n, const Vector &w) const;
+	float pdf(const Point &p, const Vector &w_i) const override;
 };
 
 #endif
--- src/lights/area_light.cpp
+++ src/lights/area_light.cpp
@@ -1,29 +1,44 @@
 #include "scene.h"
+#include "monte_carlo/util.h"
 #include "lights/area_light.h"
 
 AreaLight::AreaLight(const Transform &to_world, const Colorf &emit, int n_samples, float radius)
 	: PBRLight(to_world, n_samples), emit(emit), geometry(std::make_unique<Sphere>(radius)),
 	surface_area(geometry->surface_area())
 {}
-Colorf AreaLight::sample(const Point &p, const std::array<float, 2> &lsample,
-	Vector &wi, float &pdf_val, OcclusionTester &occlusion) const
-{
-	return Colorf{0};
+Colorf AreaLight::radiance(const Point&, const Normal &n, const Vector &w) const {
+	return w.dot(n) > 0 ? emit : Colorf{0};
 }
-Colorf AreaLight::sample(const Scene &scene, const std::array<float, 2> &a, const std::array<float, 2> &b,
+Colorf AreaLight::sample(const Point &p, const std::array<float, 2> &lsample, Vector &w_i,
+	float &pdf_val, OcclusionTester &occlusion) const
+{
+	Normal normal;
+	Point ps = geometry->sample(p, lsample, normal);
+	w_i = (ps - p).normalized();
+	pdf_val = geometry->pdf(p, w_i);
+	occlusion.set_points(p, ps);
+	return radiance(ps, normal, -w_i);
+}
+Colorf AreaLight::sample(const Scene&, const std::array<float, 2> &a, const std::array<float, 2> &b,
 	Ray &ray, Normal &normal, float &pdf_val) const
 {
+	Point o = geometry->sample(a, normal);
+	Vector d = uniform_sample_sphere(b);
+	//Make sure the ray is heading out of the surface
+	if (d.dot(normal) < 0){
+		d *= -1;
+	}
+	ray = Ray{o, d, 0.001};
+	pdf_val = geometry->pdf(o) * INV_TAU;
+	return radiance(o, normal, d);
 }
-Colorf AreaLight::power(const Scene &scene) const {
-	return Colorf{0};
+Colorf AreaLight::power(const Scene&) const {
+	return emit * surface_area * PI;
 }
 bool AreaLight::delta_light() const {
 	return false;
 }
-float AreaLight::pdf(const Point &p, const Vector &wi) const {
-	return 0;
-}
-Colorf AreaLight::radiance(const Point &p, const Normal &n, const Vector &w) const {
-	return w.dot(n) > 0 ? emit : Colorf{0};
+float AreaLight::pdf(const Point &p, const Vector &w_i) const {
+	return geometry->pdf(p, w_i);
 }
 
