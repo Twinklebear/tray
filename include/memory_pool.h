242f62811f2fe87565f65aeef65366ce0cafc3da
Will <willusher.life@gmail.com>
2014-11-07 09:31:20 -0700

    Wrote a simple memory pool, should probably test it some

---
+++ include/memory_pool.h
@@ -0,0 +1,51 @@
+#ifndef MEMORY_POOL_H
+#define MEMORY_POOL_H
+
+#include <vector>
+
+/*
+ * A memory pool that allocates memory in chunks of the specified block size.
+ * Only supports freeing of the entire pool, not individual allocations and can not
+ * be moved or copied (it's possible to implement but I don't think I need it in the project)
+ */
+class MemoryPool {
+	struct Block {
+		uint32_t size;
+		char *block;
+	};
+
+	uint32_t cur_block_pos, block_size;
+	Block cur_block;
+	std::vector<Block> used, available;
+
+public:
+	/*
+	 * Create the memory pool specifying the block size to do allocations in (default is 32k)
+	 */
+	MemoryPool(uint32_t block_size = 32768);
+	MemoryPool(const MemoryPool&) = delete;
+	MemoryPool& operator=(const MemoryPool&) = delete;
+	MemoryPool(const MemoryPool&&) = delete;
+	MemoryPool& operator=(const MemoryPool&&) = delete;
+	~MemoryPool();
+	/*
+	 * Construct a type in the memory pool
+	 */
+	template<typename T, typename... Args>
+	T* alloc(Args&&... args){
+		return new(alloc(sizeof(T))) T{std::forward<Args>(args)...};
+	}
+	/*
+	 * Clear all used blocks, making them available again
+	 */
+	void free_blocks();
+
+private:
+	/*
+	 * Allocate some number of bytes to be used for constructing some object
+	 */
+	void* alloc(uint32_t size);
+};
+
+#endif
+
---
+++ src/memory_pool.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include "memory_pool.h"
+
+MemoryPool::MemoryPool(uint32_t block_size) : cur_block_pos(0), block_size(block_size){
+	cur_block = Block{block_size, new char[block_size]};
+}
+MemoryPool::~MemoryPool(){
+	delete[] cur_block.block;
+	for (auto &b : used){
+		delete[] b.block;
+	}
+	for (auto &b : available){
+		delete[] b.block;
+	}
+}
+void MemoryPool::free_blocks(){
+	while (!used.empty()){
+		available.push_back(used.back());
+		used.pop_back();
+	}
+}
+void* MemoryPool::alloc(uint32_t size){
+	//Round size to minimum machine alignment
+	size = (size + 15) & ~15;
+	//If we need a new block to store this allocation
+	if (cur_block_pos + size > block_size){
+		used.push_back(cur_block);
+		//If we've got an available block that can fit use that, otherwise allocate a new one
+		auto block = std::find_if(available.begin(), available.end(),
+			[size](const auto &b){
+				return size <= b.size;
+			});
+		if (block != available.end()){
+			cur_block = *block;
+			available.erase(block);
+		}
+		else {
+			uint32_t sz = std::max(size, block_size);
+			cur_block = Block{sz, new char[sz]};
+		}
+		cur_block_pos = 0;
+	}
+	void *mem = cur_block.block + cur_block_pos;
+	cur_block_pos += size;
+	return mem;
+}
+
