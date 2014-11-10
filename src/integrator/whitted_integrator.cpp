fbd34860fe0f996d16902aab145ffca15f3430d1
Will <willusher.life@gmail.com>
2014-11-09 23:09:19 -0700

    Whitted should only take one sample per light

--- src/integrator/whitted_integrator.cpp
+++ src/integrator/whitted_integrator.cpp
@@ -23,26 +23,22 @@ Colorf WhittedIntegrator::illumination(const Scene &scene, const Renderer &rende
 	dg.compute_differentials(ray);
 	BSDF *bsdf = mat->get_bsdf(dg, pool);
 
+	std::array<float, 2> l_samples;
 	//Compute the incident light from all lights in the scene
 	for (const auto &l : scene.get_light_cache()){
-		int n_samples = l.second->n_samples;
-		auto l_samples = pool.alloc_array<std::array<float, 2>>(n_samples);
-		sampler.get_samples(l_samples, n_samples);
-		for (int i = 0; i < n_samples; ++i){
-			Vector w_i;
-			float pdf_val = 0;
-			OcclusionTester occlusion;
-			Colorf li = l.second->sample(bsdf->dg.point, l_samples[i], w_i, pdf_val, occlusion);
-			//If there's no light or no probability for this sample there's no illumination
-			if (li.luminance() == 0 || pdf_val == 0){
-				continue;
-			}
-			Colorf c = (*bsdf)(w_o, w_i);
-			if (!c.is_black() && !occlusion.occluded(scene)){
-				illum += c * li * std::abs(w_i.dot(bsdf->dg.normal)) / pdf_val;
-			}
+		sampler.get_samples(&l_samples, 1);
+		Vector w_i;
+		float pdf_val = 0;
+		OcclusionTester occlusion;
+		Colorf li = l.second->sample(bsdf->dg.point, l_samples, w_i, pdf_val, occlusion);
+		//If there's no light or no probability for this sample there's no illumination
+		if (li.luminance() == 0 || pdf_val == 0){
+			continue;
 		}
-		illum /= n_samples;
+		Colorf c = (*bsdf)(w_o, w_i);
+		if (!c.is_black() && !occlusion.occluded(scene)){
+			illum += c * li * std::abs(w_i.dot(bsdf->dg.normal)) / pdf_val;
+		}
 	}
 	if (ray.depth < max_depth){
 		illum += spec_reflect(ray, *bsdf, renderer, scene, sampler, pool);
