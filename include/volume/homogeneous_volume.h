687a21866b860a5515224a805002b90ab0bcf059
Will Usher <willusher.life@gmail.com>
2014-12-07 19:08:52 -0700

    Adding basic homogeneous volume

---
+++ include/volume/homogeneous_volume.h
@@ -0,0 +1,68 @@
+#ifndef HOMOGENEOUS_VOLUME_H
+#define HOMOGENEOUS_VOLUME_H
+
+#include "volume.h"
+
+/*
+ * A very simple volume, describes a box in space with
+ * homogeneous scattering properties throughout
+ */
+class HomogeneousVolume : public Volume {
+	//Scattering, absoprtion, emission coefficients describing the volume
+	Colorf sig_a, sig_s, emit;
+	float phase_asymmetry;
+	//The region that the volume occupies
+	BBox region;
+
+public:
+	/*
+	 * Construct the homgeneous volume specifying its properties and bounds
+	 */
+	HomogeneousVolume(const Colorf &sig_a, const Colorf &sig_s, const Colorf &emit,
+		float phase_asymmetry, const BBox &region);
+	/*
+	 * Get the object-space bounds for the volume region
+	 */
+	BBox bound() const override;
+	/*
+	 * Test if the ray intersects the volume region, returning the min/max t
+	 * values that the ray overlapped the region in
+	 */
+	bool intersect(Ray &ray) const override;
+	/*
+	 * Get the absorption coefficient of the volume at some point
+	 * along the direction passed
+	 */
+	Colorf absorption(const Point &p, const Vector &v) const override;
+	/*
+	 * Get the scattering coefficient of the volume at some point
+	 * along the direction passed
+	 */
+	Colorf scattering(const Point &p, const Vector &v) const override;
+	/*
+	 * Get the attenuation coefficient of the volume at some poiint
+	 * along the direciton passed
+	 */
+	Colorf attenuation(const Point &p, const Vector &v) const;
+	/*
+	 * Get the emission property of the volume region at some point
+	 * along the direction passed
+	 */
+	Colorf emission(const Point &p, const Vector &v) const override;
+	/*
+	 * Get the optical thickness of the region along the ray on the line
+	 * segment from min_t to max_t
+	 * For volumes that use Monte Carlo integration to compute this value step
+	 * and offset are used to control how to step along the segment to estimate
+	 * the integral
+	 */
+	Colorf optical_thickness(const Ray &ray, float step = 1, float offset = 0.5) const override;
+	/*
+	 * Get the phase function value for the volume at the some point
+	 * for a ray incident along w_i and exiting along w_o
+	 */
+	float phase(const Point &p, const Vector &w_i, const Vector &w_o) const override;
+};
+
+#endif
+
---
+++ src/volume/homogeneous_volume.cpp
@@ -0,0 +1,36 @@
+#include <array>
+#include "volume/homogeneous_volume.h"
+
+HomogeneousVolume::HomogeneousVolume(const Colorf &sig_a, const Colorf &sig_s, const Colorf &emit,
+	float phase_asymmetry, const BBox &region)
+	: sig_a(sig_a), sig_s(sig_s), emit(emit), phase_asymmetry(phase_asymmetry), region(region)
+{}
+BBox HomogeneousVolume::bound() const {
+	return region;
+}
+bool HomogeneousVolume::intersect(Ray &ray) const {
+	return region.intersect(ray, &ray.min_t, &ray.max_t);
+}
+Colorf HomogeneousVolume::absorption(const Point &p, const Vector&) const {
+	return region.inside(p) ? sig_a : 0;
+}
+Colorf HomogeneousVolume::scattering(const Point &p, const Vector&) const {
+	return region.inside(p) ? sig_s : 0;
+}
+Colorf HomogeneousVolume::attenuation(const Point &p, const Vector&) const {
+	return region.inside(p) ? sig_a + sig_s : 0;
+}
+Colorf HomogeneousVolume::emission(const Point &p, const Vector&) const {
+	return region.inside(p) ? emit : 0;
+}
+Colorf HomogeneousVolume::optical_thickness(const Ray &ray, float, float) const {
+	std::array<float, 2> t;
+	if (!region.intersect(ray, &t[0], &t[1])){
+		return 0;
+	}
+	return ray(t[0]).distance(ray(t[1])) * (sig_a + sig_s);
+}
+float HomogeneousVolume::phase(const Point &p, const Vector &w_i, const Vector &w_o) const {
+	return region.inside(p) ? phase_henyey_greenstein(w_i, w_o, phase_asymmetry) : 0;
+}
+
