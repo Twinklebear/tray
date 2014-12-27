32e315b83f23e5219e5819f6198c043b17350b22
Will Usher <willusher.life@gmail.com>
2014-12-27 14:19:38 -0800

    No need to set those 0's since they're already in the identity mat

--- src/linalg/transform.cpp
+++ src/linalg/transform.cpp
@@ -83,22 +83,15 @@ Transform Transform::rotate(const Vector &axis, float deg){
 	m[0][0] = a.x * a.x + (1 - a.x * a.x) * c;
 	m[0][1] = a.x * a.y * (1 - c) - a.z * s;
 	m[0][2] = a.x * a.z * (1 - c) + a.y * s;
-	m[0][3] = 0;
 
 	m[1][0] = a.x * a.y * (1 - c) + a.z * s;
 	m[1][1] = a.y * a.y + (1 - a.y * a.y) * c;
 	m[1][2] = a.y * a.z * (1 - c) - a.x * s;
-	m[1][3] = 0;
 
 	m[2][0] = a.x * a.z * (1 - c) - a.y * s;
 	m[2][1] = a.y * a.z * (1 - c) + a.x * s;
 	m[2][2] = a.z * a.z + (1 - a.z * a.z) * c;
-	m[2][3] = 0;
 
-	m[3][0] = 0;
-	m[3][1] = 0;
-	m[3][2] = 0;
-	m[3][3] = 1;
 	return Transform{m, m.transpose()};
 }
 Transform Transform::look_at(const Point &pos, const Point &center, const Vector &up){
